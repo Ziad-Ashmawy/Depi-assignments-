df15bf3b67ac0d3fb3f9bb34d8e936dcd25f80fa
Ziad Ashmawy <ziadashmawy817@gmail.com>
2024-12-06 08:44:26 +0200

    Add files via upload

---
+++ Assignment 5.sql
@@ -0,0 +1,35 @@
+/*                         Assignment 5              */
+/*Q1*/
+select * from employees;
+
+select employee_id,first_name,last_name,department_id from employees 
+select * from locations
+select * from departments
+
+/*Q2*/
+select distinct department_id from employees
+
+/*Q3*/
+create table student (ID int  primary key ,First_Name char(50) not null,Last_name Char(50) default'Unknown',Address char(50) default 'N/A',
+Birth_Date date);
+Alter table student 
+Add city varchar(30);
+
+drop table student
+
+/*Q4*/
+insert into student (id,first_name , last_name, address,city,birth_date)
+values (1,'Ahmed','Ali','Downtown','Cairo','1955-01-01')
+
+update student set address = 'Garden City'
+where First_name = 'Ahmed';
+
+
+
+/*Q5*/
+
+delete from student 
+where  city ='cairo';
+rollback;
+select * from student;
+
